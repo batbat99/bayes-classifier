9b664313af1f83f5fc17e1ea10601854a2c7a558
batbat99 <hbatayneh@outlook.com>
2022-06-17 00:19:54 +0200

    implemented a bayes classifier

---
+++ bayes_classifier.py
@@ -0,0 +1,55 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.neighbors import KernelDensity
+import numpy as np
+import matplotlib.pyplot as plt
+
+class KDEClassifier(BaseEstimator, ClassifierMixin):
+    """Bayesian generative classification based on KDE
+    
+    Parameters
+    ----------
+    bandwidth : float
+        the kernel bandwidth within each class
+    kernel : str
+        the kernel name, passed to KernelDensity
+    """
+    def __init__(self, bandwidth=1.0, kernel='gaussian'):
+        self.bandwidth = bandwidth
+        self.kernel = kernel
+        
+    def fit(self, X, y):
+        self.classes_ = np.sort(np.unique(y))
+        training_sets = [X[y == yi] for yi in self.classes_]
+        self.models_ = [KernelDensity(bandwidth=self.bandwidth,
+                                      kernel=self.kernel).fit(Xi)
+                        for Xi in training_sets]
+        self.logpriors_ = [np.log(Xi.shape[0] / X.shape[0])
+                           for Xi in training_sets]
+        return self
+        
+    def predict_proba(self, X):
+        logprobs = np.array([model.score_samples(X)
+                             for model in self.models_]).T
+        result = np.exp(logprobs + self.logpriors_)
+        return result / result.sum(1, keepdims=True)
+        
+    def predict(self, X):
+        return self.classes_[np.argmax(self.predict_proba(X), 1)]
+
+from sklearn.datasets import load_digits, load_wine, load_breast_cancer, load_iris
+from sklearn.model_selection import GridSearchCV
+
+digits = load_breast_cancer()
+
+bandwidths = 10 ** np.linspace(0, 2, 100)
+grid = GridSearchCV(KDEClassifier(), {'bandwidth': bandwidths})
+grid.fit(digits.data, digits.target)
+
+scores = grid.cv_results_.get('mean_test_score').tolist()
+
+plt.semilogx(bandwidths, scores)
+plt.xlabel('bandwidth')
+plt.ylabel('accuracy')
+plt.title('KDE Model Performance')
+print(grid.best_params_)
+print('accuracy =', grid.best_score_)
