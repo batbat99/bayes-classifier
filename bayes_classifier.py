00fe264be6312df561099e6533da1c4e288be3f1
batbat99 <hbatayneh@outlook.com>
2022-06-17 00:46:39 +0200

    added features (number of training samples, shuffling data, novelty class)

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -13,13 +13,24 @@ class KDEClassifier(BaseEstimator, ClassifierMixin):
     kernel : str
         the kernel name, passed to KernelDensity
     """
-    def __init__(self, bandwidth=1.0, kernel='gaussian'):
+    def __init__(self, bandwidth=1.0, kernel='gaussian', n=None, shuffle=False, novelty=0):
         self.bandwidth = bandwidth
         self.kernel = kernel
+        self.n = n
+        self.shuffle = shuffle
+        self.novelty = novelty
         
     def fit(self, X, y):
+        X = X[y != 2]
+        y = y[y != 2]        
         self.classes_ = np.sort(np.unique(y))
         training_sets = [X[y == yi] for yi in self.classes_]
+        if self.novelty:
+            self.classes_ = np.hstack((self.classes_, np.array([2])))
+        if self.shuffle:
+            training_sets = [training_set.sample(frac=1).reset_index(drop=True) for training_set in training_sets]
+        if self.n:
+            training_sets = [training_set[:self.n] for training_set in training_sets]
         self.models_ = [KernelDensity(bandwidth=self.bandwidth,
                                       kernel=self.kernel).fit(Xi)
                         for Xi in training_sets]
@@ -34,22 +45,9 @@ class KDEClassifier(BaseEstimator, ClassifierMixin):
         return result / result.sum(1, keepdims=True)
         
     def predict(self, X):
-        return self.classes_[np.argmax(self.predict_proba(X), 1)]
+        proba = self.predict_proba(X)
+        print(proba, proba.shape)
+        if self.novelty:
+            proba = np.hstack((proba, np.ones((proba.shape[0], 1), dtype=proba.dtype) * self.novelty))
+        return self.classes_[np.argmax(proba, 1)]
 
-from sklearn.datasets import load_digits, load_wine, load_breast_cancer, load_iris
-from sklearn.model_selection import GridSearchCV
-
-digits = load_breast_cancer()
-
-bandwidths = 10 ** np.linspace(0, 2, 100)
-grid = GridSearchCV(KDEClassifier(), {'bandwidth': bandwidths})
-grid.fit(digits.data, digits.target)
-
-scores = grid.cv_results_.get('mean_test_score').tolist()
-
-plt.semilogx(bandwidths, scores)
-plt.xlabel('bandwidth')
-plt.ylabel('accuracy')
-plt.title('KDE Model Performance')
-print(grid.best_params_)
-print('accuracy =', grid.best_score_)
